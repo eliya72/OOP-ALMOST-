8078161c5c040c2a7d94fc56c5facdb9e97b7af7
Elch <lerman.eliya@gmail.com>
2022-10-06 15:44:05 +0200

    done

--- Task_3/lab_5.cpp
+++ Task_3/lab_5.cpp
@@ -7,63 +7,52 @@ private:
   float out_radius;
 
 public:
-  Smile(float r, float R)
-  {
+  Smile(float r, float R){
     in_radius = r;
     out_radius = R;
   }
 
-  Smile()
-  {
+  Smile(){
     in_radius = 0;
     out_radius = 0;
   }
 
-  auto getArea() -> float
-  {
+  auto getArea() -> float{
     return ((PI * out_radius * out_radius)-(2 * (PI * in_radius * in_radius)));
   }
 
-  auto getIn_Radius() -> float
-  {
+  auto getIn_Radius() -> float{
     return in_radius;
   }
 
-  auto getOut_Radius() -> float
-  {
+  auto getOut_Radius() -> float{
     return out_radius;
   }
 
-  void setIn_Radius(float r)
-  {
+  void setIn_Radius(float r){
     in_radius = r;
   }
 
-  void setOut_Radius(float R)
-  {
+  void setOut_Radius(float R){
     out_radius = R;
   }
 
-  void print_over()
-  {
+  void print_over(){
     std::cout << "in radius: " << getIn_Radius() << ";  out radius: " << getOut_Radius() << ";  Area: " << getArea() << std::endl;
   }
 
-  Smile operator+(Smile &object)
-  {
+  Smile operator+(Smile &object){
     Smile new_smile(in_radius + object.getIn_Radius(), out_radius + object.getOut_Radius());
     return new_smile;
   }
 
-  Smile operator++()
-  {
+  Smile operator++(){
     in_radius++;
     out_radius++;
     return *this;
   }
 
-  Smile operator++(int a)
-  {
+  Smile operator++(int a){
     Smile for_pointer = *this;
     in_radius++;
     out_radius++;
@@ -72,14 +61,12 @@ public:
   friend std::ostream &operator << (std::ostream &for_pointer, Smile &obj);
 };
 
-std::ostream &operator << (std::ostream &for_pointer, Smile &obj)
-{
+std::ostream &operator << (std::ostream &for_pointer, Smile &obj){
   for_pointer << "In radius: " << obj.getIn_Radius() << ";  Out radius: " << obj.getOut_Radius() << ";  Area: " << obj.getArea();
   return for_pointer;
 }
 
-int main()
-{
+int main(){
   Smile rad_Rad_1(2, 4);
   Smile rad_Rad_2(2, 4);
   Smile rad_Rad_sum = rad_Rad_1 + rad_Rad_2;
