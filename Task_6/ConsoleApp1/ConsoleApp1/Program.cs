3bec5c1c332fc4e1be8b575b7684f7ee50772e3d
Elch <lerman.eliya@gmail.com>
2022-10-27 13:44:52 +0200

    done

--- Task_6/ConsoleApp1/ConsoleApp1/Program.cs
+++ Task_6/ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,27 @@ namespace MyApplication1
     {
         static void Main(string[] args)
         {
-
+            double kinetic, potential;
+            double m, t, v, g=9.8, h;
+            Console.WriteLine("Enter m value:");
+            m = Convert.ToInt32(Console.ReadLine());
+            if (m<0) {
+                Console.WriteLine($"\nYour m value is negative");
+                Environment.Exit(0);
+            }
+            Console.WriteLine("Enter v value:");
+            v = Convert.ToInt32(Console.ReadLine());
+            kinetic = (m*v*v)/2;
+            Console.WriteLine("Enter h value:");
+            h = Convert.ToInt32(Console.ReadLine());
+            if (h<0)
+            {
+                Console.WriteLine($"\nYour h value is negative");
+                Environment.Exit(0);
+            }
+            potential = m*g*h;
+            Console.WriteLine($"\nYour kinetic energy is:\t{kinetic}\n");
+            Console.WriteLine($"\nYour potential energy is: {potential}\n");
         }
     }
 }
